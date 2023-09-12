80e87205523450b3b744dfab3ca692c3c0df8543
carrotisorange <thepropertymanager2020@gmail.com>
2023-09-12 20:41:38 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import Message from "/src/Message";
+import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+
+function App(){
+  // const name = 'John';
+  return <div>
+    <ListGroup />
+    <Alert />
+    <Button />
+  </div>
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Button = () => {
+  return (
+    <div>
+        <button type="button" className="btn btn-primary">Button</button>
+    </div>
+  )
+}
+
+export default Button;
